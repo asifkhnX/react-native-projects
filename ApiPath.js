9484c6cfd7e55970ed5623d7704304f86f1e21a6
asifkhnX <144679315+asifkhnX@users.noreply.github.com>
2025-09-16 13:00:39 +0500

    Delete ApiPath.js

--- ApiPath.js
+++
@@ -1,5 +0,0 @@
-// export const API_PATH = 'http://192.168.100.11:3000/api'
-// export const FILE_PATH = 'http://192.168.100.11:3000'
-
-export const API_PATH = 'https://appadmin.sswellness.com.my/api'
-export const FILE_PATH = 'https://appadmin.sswellness.com.my/api/uploads'
